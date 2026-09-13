0fe9ee1a159b7c31bd9b89b69bf4769f46219f66
nodirbekaminov <nodiraminov0101@gmail.com>
2026-09-13 15:11:08 +0500

    feat: learn numbers in python

---
+++ primitive.py
@@ -0,0 +1,16 @@
+print("======= number ========")
+
+# in JAVA, variable  is a name storage location
+# in PYTHON, variables is names reference
+
+
+count = 100
+count_type = type(count)
+print("count:", count, count_type)
+print(f"The count: {count}  and type {count_type}")
+
+
+result1 = count.bit_count()   #method
+result2 = count.numerator    #state
+
+print(result1, result2)
