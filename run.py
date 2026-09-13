2d8ea8afa4e4e18c9e62bd4b6c4b51d08f24dc1f
nodirbekaminov <nodiraminov0101@gmail.com>
2026-09-13 15:02:33 +0500

    feat: python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-print("Hello, World!")
+# Dunder __builtins__, __init__
 
-print("This is a simple Python script.")
+
+message = ("PYTHON: Everything is object!")
+print(message)
+
+result = type(message)
+print("result:", result)
+
+'''
+In Python, there are bultin tools:
+(1). TYPES => int, float, str, list, dict
+(2). FUNCTION => print(), len(), input(), type()
+(3). CONSTANTS => TRUE, FALSE, NONE
+'''
+
+
+# print(dir(__builtins__))
