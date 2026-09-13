06d015f165ba0f42a590962415cca45a9a34fbed
nodirbekaminov <nodiraminov0101@gmail.com>
2026-09-13 15:23:06 +0500

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -14,3 +14,25 @@ result1 = count.bit_count()   #method
 result2 = count.numerator    #state
 
 print(result1, result2)
+
+
+print("======= string ========")
+# METHODS: upper(), lower(), title(), find(), replace()
+course = "AI PYTHON FULLSTACK"
+result = type(course)
+# print(f"The type of course: {result}")
+
+result = course.title()
+print(f"The result (1): {result}")
+
+result = course.upper ()
+print(f"The result (2): {result}")
+
+
+result = course.replace("FULLSTACK", "MasterClass")
+print(f"The result (3): {result}")
+print(course)
+
+# course = course.replace("FULLSTACK", "MasterClass")
+# print(f"The result (3): {result}")
+# print(course)
